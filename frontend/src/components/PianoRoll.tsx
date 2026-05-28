2c65eadfcf3d3a11fc1daf1166cb2c7dd9a8c689
xuxi <showerbandv@gmail.com>
2026-05-28 22:08:18 +0800

    Expand piano roll pitch range: C6-C2 (4 octaves, 49 pitches)

--- frontend/src/components/PianoRoll.tsx
+++ frontend/src/components/PianoRoll.tsx
@@ -21,8 +21,10 @@ interface PianoRollProps {
 
 // Fixed note rows on our key matrix (C3 up to C5)
 const PIANO_ROWS = [
-  "C5", "B4", "A#4", "A4", "G#4", "G4", "F#4", "F4", "E4", "D#4", "D4", "C#4", "C4",
-  "B3", "A#3", "A3", "G#3", "G3", "F#3", "F3", "E3", "D#3", "D3", "C#3", "C3"
+  "C6", "B5", "A#5", "A5", "G#5", "G5", "F#5", "F5", "E5", "D#5", "D5", "C#5", "C5",
+  "B4", "A#4", "A4", "G#4", "G4", "F#4", "F4", "E4", "D#4", "D4", "C#4", "C4",
+  "B3", "A#3", "A3", "G#3", "G3", "F#3", "F3", "E3", "D#3", "D3", "C#3", "C3",
+  "B2", "A#2", "A2", "G#2", "G2", "F#2", "F2", "E2", "D#2", "D2", "C#2", "C2"
 ];
 
 // Determine if row index matches a black key on piano keyboard
